e4be328a886389e2dbde5ba0098b02354f2bc3c3
user <carola1408@yahoo.com.tw>
2023-06-20 15:22:56 +0800

    feat: add bcrypt.js

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const db = require('../../models')
 const User = db.User
 //設定登入路由
@@ -20,27 +21,27 @@ router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   // 檢查使用者是否已經註冊
   User.findOne({ email }).then(user => {
-    // 如果已經註冊：退回原本畫面
     if (user) {
-      console.log('User already exists.')
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
+    }
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
         name,
         email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+        password: hash // 用雜湊值取代原本的使用者密碼
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
-    .catch(err => console.log(err))
 })
 
 //設定登出路由
