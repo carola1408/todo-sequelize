cfd8002cc2aff70a4cab0f2129edf793778f7c97
user <carola1408@yahoo.com.tw>
2023-06-20 10:38:11 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const exphbs = require('express-handlebars')
-// const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
@@ -18,8 +17,6 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-// app.use(bodyParser.urlencoded({ extended: true }))
 // setting body-parser 改寫成 express，也可以直接取得 
 app.use(express.urlencoded({ extended: true }))
 
@@ -42,7 +39,7 @@ app.get('/todos/new', (req, res) => {
 //資料庫新增資料
 app.post('/todos', (req, res) => {
   const name = req.body.name  // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })    // 存入資料庫
+  return Todo.create({ name })   // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
